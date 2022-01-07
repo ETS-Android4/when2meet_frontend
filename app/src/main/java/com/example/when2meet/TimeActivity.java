ef520c2f897965b5e9be3facd4ef0ec462bc35ce
Jungwoojin <woojin7879@unist.ac.kr>
2022-01-08 02:20:30 +0900

    time finish

--- app/src/main/java/com/example/when2meet/TimeActivity.java
+++ app/src/main/java/com/example/when2meet/TimeActivity.java
@@ -1,14 +1,22 @@
 package com.example.when2meet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class TimeActivity extends AppCompatActivity {
 
+    TimeAdapter timeadapter;
+    RecyclerView toggles;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
+        toggles = findViewById(R.id.toggle_rcv);
+        timeadapter = new TimeAdapter();
+        toggles.setAdapter(timeadapter);
+
     }
 }
